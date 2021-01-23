effb0fe0d6238c85803c7d6086da44a88d7d50fc
indrapreet singh <indrapreetsingh41@gmail.com>
2021-01-23 11:59:34 +0000

    Reduce LOC.

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const input = readline.question("Welcome to UrlEncodeNdDecode.\n\nEnter Your Str
 
 const choice = readline.question("\nEnter 1 to Encode\nEnter 2 to Decode\n");
 
-encodeURI => encodeURIComponent(input);
+const encodeURI = (input) => encodeURIComponent(input);
 
-decodeURI => decodeURIComponent(input);
+const decodeURI = (input) => decodeURIComponent(input);
 
 if(choice==1)
-{
-  console.log("Encoded URI-\n"+encodeURI(input));
-}
+console.log("Encoded URI-\n"+encodeURI(input));
+
 else if(choice==2)
-{
-  console.log("Decoded URI-\n"+decodeURI(input));
-}
+console.log("Decoded URI-\n"+decodeURI(input));
 
 else
 console.log("Enter a Valid Option!");
